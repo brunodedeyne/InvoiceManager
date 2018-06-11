9c05d70c99bdd81471e51a9bfa065dbb9036ba62
Bruno Dedeyne <brunodedeyne@MacBook-Pro-van-Bruno.local>
2018-06-11 11:36:19 +0200

    bug

--- Mobile/invoicemanager/App.js
+++ Mobile/invoicemanager/App.js
@@ -12,15 +12,6 @@ import {
   Spinner
 } from 'native-base';
 
-var config = {
-  apiKey: "AIzaSyDiYwctQZs8cq4LwrUJ0JZvs0ne2f9Bjbg",
-  authDomain: "invoicemanager-1525702104034.firebaseapp.com",
-  databaseURL: "https://invoicemanager-1525702104034.firebaseio.com",
-  projectId: "invoicemanager-1525702104034",
-  storageBucket: "invoicemanager-1525702104034.appspot.com",
-  messagingSenderId: "908003589667"
-};
-
 firebase.initializeApp(config);
 
 type Props = {};
--- Web/invoicemanager/src/App.js
+++ Web/invoicemanager/src/App.js
@@ -61,14 +61,7 @@ import './App.css';
 injectTapEventPlugin();
 
 // Database Config + init Database
-var config = {
-  apiKey: "AIzaSyDiYwctQZs8cq4LwrUJ0JZvs0ne2f9Bjbg",
-  authDomain: "invoicemanager-1525702104034.firebaseapp.com",
-  databaseURL: "https://invoicemanager-1525702104034.firebaseio.com",
-  projectId: "invoicemanager-1525702104034",
-  storageBucket: "invoicemanager-1525702104034.appspot.com",
-  messagingSenderId: "908003589667"
-};
+
 firebase.initializeApp(config);
 
 // Set DrawerWidth
